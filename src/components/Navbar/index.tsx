443d84c233292f5cd9316ef0d885acf7a8362349
Rafi Ferdos <rafiferdos@gmail.com>
2025-04-08 23:36:42 +0600

    feat: add Navbar component and implement Sheet UI components

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  const Tabs = [
+    { name: 'Home', path: '/' },
+    { name: 'Products', path: '/products' },
+    { name: 'About', path: '/about' },
+  ]
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Navbar;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+
 import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
