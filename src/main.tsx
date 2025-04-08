90db57ab78b4c14790a217e25369bf62cdcf1b9b
Rafi Ferdos <rafiferdos@gmail.com>
2025-04-08 23:25:30 +0600

    feat: wrap application in ThemeProvider and ScrollContainer components

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './providers/theme-provider.tsx'
+import { ScrollContainer } from 'react-nice-scroll'
+import 'react-nice-scroll/dist/styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
+    <ThemeProvider>
+      <ScrollContainer>
+        <RouterProvider router={router} />
+      <Toaster />
+      </ScrollContainer>
+    </ThemeProvider>
   </StrictMode>,
 )
