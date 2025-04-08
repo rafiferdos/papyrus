72f3255eb88af3a9333e650a5edad02e3954e9e4
Rafi Ferdos <rafiferdos@gmail.com>
2025-04-08 23:32:08 +0600

    feat: implement routing with MainLayout and Home components

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { ThemeProvider } from './providers/theme-provider.tsx'
 import { ScrollContainer } from 'react-nice-scroll'
 import 'react-nice-scroll/dist/styles.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/index.tsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <ScrollContainer>
         <RouterProvider router={router} />
-      <Toaster />
+        <Toaster />
       </ScrollContainer>
     </ThemeProvider>
-  </StrictMode>,
+  </StrictMode>
 )
---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import MainLayout from "@/layouts/MainLayout";
+import Home from "@/pages/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      }
+    ]
+  }
+])
+
+export default router;
