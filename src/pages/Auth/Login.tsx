58b9a7c851995b45adc75a9263b16b9a621d8b4e
mirzanahid <mirzasaikat3@gmail.com>
2025-04-10 19:33:51 +0600

    fix navbar on top

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,20 +1,19 @@
-import LoginForm from '@/components/forms/LoginForm'
-import { TextEffect } from '@/components/ui/text-effect'
-
+import LoginForm from "@/components/forms/LoginForm";
+import { TextEffect } from "@/components/ui/text-effect";
 
 const Login: React.FC = () => {
   return (
-    <div className='flex items-center justify-center lg:min-h-[calc(100vh-5rem)] flex-col mt-12 shadow-lg'>
-      <div className='text-center text-4xl font-bold my-8'>
-        <TextEffect preset='blur' speedReveal={1.1} speedSegment={0.3}>
+    <div className="flex items-center justify-center lg:min-h-[calc(100vh-5rem)] flex-col mt-12 shadow-lg">
+      <div className="text-center text-4xl font-bold my-8">
+        <TextEffect preset="blur" speedReveal={1.1} speedSegment={0.3}>
           Sign In
         </TextEffect>
       </div>
-      <div className='max-w-lg mx-auto w-full'>
+      <div className="max-w-lg mx-auto w-full">
         <LoginForm />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
